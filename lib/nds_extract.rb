b7411fd576a4b7ae91f76e4bc2a0aae7053276a3
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-27 23:34:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,14 +10,14 @@ def directors_totals(nds)
 result = {}
 director_index = 0
 while director_index < nds.length do
-  director = nds[directorjono][:name]
+  director = nds[director_index][:name]
    movie_index = 0
    result[director] = 0
-   while movie_index < nds[directorjono[:movies].length do
-     result[director] += nds[directorjono][:movies][movie_index][:worldwide_gross]
+   while movie_index < nds[director_index[:movies].length do
+     result[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
-    director_jono += 1
+    director_index += 1
   end
   result
 
